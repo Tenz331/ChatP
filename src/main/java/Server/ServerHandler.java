53e14dcbee850b50c7232e605617ec8e942af67b
Christian Gram Kalhauge <christian@kalhauge.dk>
2020-10-02 11:08:48 +0200

    Clean serverhandler

--- src/main/java/Server/ServerHandler.java
+++ src/main/java/Server/ServerHandler.java
@@ -6,36 +6,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class ServerHandler implements Runnable {
-    private final BufferedReader in;
+public class ServerHandler {
     private static final int PORT = 2222;
     private static final ArrayList<ClientHandler> clientHandlers = new ArrayList<>(); //client threads
 
-    public ServerHandler(Socket socket) throws IOException {
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    }
-
-    @Override
-    public void run() { //thread
-        String serverResponds;
-        try {
-            while (true) {
-                serverResponds = in.readLine();
-                if (serverResponds == null) break;
-                String serverHandle = ">";
-                System.out.println(serverHandle + " " + serverResponds); //message bounce back
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
     public static void main(String[] args) throws IOException{
         ServerSocket listener = new ServerSocket(PORT);
         System.out.println("[SERVER] listening on port: " + PORT); // starting server
